91e85b146c34873d93637a30167328af0df5ec44
Youssef Shoala <youssefshoala@hotmail.com>
2023-12-04 20:44:35 -0500

    commit

--- convert_csv_to_mat.m
+++ convert_csv_to_mat.m
@@ -1,8 +1,10 @@
 % path to csv data
-csvFilePath = "Z:\Desktop\Courses\ECE1508-Cybersecurity\final project\Car_Hacking_Challenge_Dataset_rev20Mar2021\0_Preliminary\1_Submission\Pre_submit_D.csv";
+csvFilePath = "C:\Users\youss\Desktop\IDS-for-EV\Car_Hacking_Challenge_Dataset_rev20Mar2021\0_Preliminary\1_Submission\Pre_submit_D.csv";
 
 % read file
-data = readtable(csvFilePath);
+opts = detectImportOptions(csvFilePath)
+disp(opts)
+data = readtable(csvFilePath, opts);
 
 matFileName = 'pre_submit_d.mat';
 save(matFileName, 'data')

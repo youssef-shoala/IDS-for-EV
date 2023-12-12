6c522aeaef4e803dcdc187ac54a55e479c7e0a70
huyang531 <yanghuu531@gmail.com>
2023-12-11 23:49:57 -0500

    Update convert_csv_to_mat.m

--- convert_csv_to_mat.m
+++ convert_csv_to_mat.m
@@ -1,9 +1,10 @@
 % path to csv data
 csvFilePath = "C:\Users\youss\Desktop\IDS-for-EV\Car_Hacking_Challenge_Dataset_rev20Mar2021\0_Preliminary\1_Submission\Pre_submit_D.csv";
+% csvFilePath = "Z:\Desktop\Courses\ECE1508-Cybersecurity\final project\Car_Hacking_Challenge_Dataset_rev20Mar2021\0_Preliminary\1_Submission\Pre_submit_D.csv";
 
 % read file
-opts = detectImportOptions(csvFilePath)
-disp(opts)
+opts = detectImportOptions(csvFilePath);
+opts = setvartype(opts, 'Arbitration_ID', 'char');
 data = readtable(csvFilePath, opts);
 
 matFileName = 'pre_submit_d.mat';
